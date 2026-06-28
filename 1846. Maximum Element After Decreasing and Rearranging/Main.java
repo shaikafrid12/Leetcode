56887a525de2ee8e36306d0421d2563e6618acba
Shaik Afrid <shaikafrid709@gmail.com>
2026-06-28 18:40:57 +0530

    docs & feat: add solution and fix challenge description for 1846

---
+++ 1846. Maximum Element After Decreasing and Rearranging/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maximumElementAfterDecrementingAndRearranging(arr));
+        sc.close();
+    }
+
+    public static int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        }
+        return arr[arr.length - 1];
+    }
+}
